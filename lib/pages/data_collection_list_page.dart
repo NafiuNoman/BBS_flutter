28fa339ef0c7a34c46f2f19222d51b87215d0d6b
nafiunoman <nafiunoman@gmail.com>
2022-12-07 17:36:14 +0600

    table 50% done

--- lib/pages/data_collection_list_page.dart
+++ lib/pages/data_collection_list_page.dart
@@ -6,6 +6,64 @@ class DataCollectionListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var dataList = [
+      {
+        "serial": "1",
+        "date": "22.02.2022",
+        "Enumeration_Area": "01",
+        "Household_Number": "045",
+        "Type": "General",
+        "Male": "03",
+        "Female": "06",
+        "Status": "Submitted"
+      },
+      {
+        "serial": "2",
+        "date": "23.05.2022",
+        "Enumeration_Area": "05",
+        "Household_Number": "075",
+        "Type": "Extreme",
+        "Male": "66",
+        "Female": "16",
+        "Status": "Approved"
+      },
+      {
+        "serial": "3",
+        "date": "12.02.2022",
+        "Enumeration_Area": "01",
+        "Household_Number": "045",
+        "Type": "Easy",
+        "Male": "09",
+        "Female": "05",
+        "Status": "Recommended"
+      },
+      {
+        "serial": "4",
+        "date": "22.02.2022",
+        "Enumeration_Area": "01",
+        "Household_Number": "045",
+        "Type": "Normal",
+        "Male": "59",
+        "Female": "29",
+        "Status": "Approved"
+      },
+      {
+        "serial": "5",
+        "date": "22.02.2022",
+        "Enumeration_Area": "01",
+        "Household_Number": "045",
+        "Type": "Intermediate",
+        "Male": "27",
+        "Female": "94",
+        "Status": "Submitted"
+      }
+    ];
+
+    // void makeCell() {
+    //   dataList.map((obj) => print(obj));
+    //   print('aaaaaaaaa');
+    // }
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -21,15 +79,17 @@ class DataCollectionListPage extends StatelessWidget {
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: DataTable(
-
                   columnSpacing: 15,
                   headingRowHeight: 30,
                   dataRowHeight: 40,
-                  headingTextStyle: TextStyle(fontSize: 8,),
-                  dataTextStyle: const TextStyle(fontSize: 8,color: tableDataTextColor),
-
+                  headingTextStyle: TextStyle(
+                    fontSize: 8,
+                  ),
+                  dataTextStyle:
+                      const TextStyle(fontSize: 8, color: tableDataTextColor,),
                   showBottomBorder: true,
-                  headingRowColor: MaterialStateProperty.all(tableHeadingRowFillColor),
+                  headingRowColor:
+                      MaterialStateProperty.all(tableHeadingRowFillColor),
                   dataRowColor: MaterialStateProperty.all(tableRowFillColor),
                   border: TableBorder(
                     borderRadius: BorderRadius.circular(3),
@@ -39,51 +99,80 @@ class DataCollectionListPage extends StatelessWidget {
                     right: borderSide,
                     horizontalInside: borderSide,
                     verticalInside: borderSide,
-
                   ),
-
-                  columns: const [
-                    DataColumn(label: Expanded(child: Text('Serial no'))),
+                  columns: [
+                    DataColumn(
+                      label: Expanded(
+                        child: Text('Serial'),
+                      ),
+                    ),
                     DataColumn(label: Expanded(child: Text('Date'))),
-                    DataColumn(label: Expanded(child: Text('Enumeration Area'))),
-                    DataColumn(label: Expanded(child: Text('Household number'))),
+                    DataColumn(
+                        label: Expanded(child: Text('Enumeration Area'))),
+                    DataColumn(
+                        label: Expanded(child: Text('Household number'))),
                     DataColumn(label: Expanded(child: Text('Type'))),
+                    DataColumn(label: Expanded(child: Text('Male'))),
+                    DataColumn(label: Expanded(child: Text('Female'))),
+                    DataColumn(label: Expanded(child: Text('Status'))),
+                    DataColumn(
+                        label: Expanded(
+                            child: Icon(
+                      Icons.menu,
+                      size: 15,
+                      color: Colors.white,
+                    ))),
                     ////
-                    DataColumn(label: Expanded(child: Text('Serial no'))),
-                    DataColumn(label: Expanded(child: Text('Date'))),
-                    DataColumn(label: Expanded(child: Text('Enumeration Area'))),
-                    DataColumn(label: Expanded(child: Text('Household number'))),
-                    DataColumn(label: Expanded(child: Text('Type'))),
+                  ],
+                  rows: dataList
+                      .map(
+                        (obj) => DataRow(
+                          cells: [
+                            DataCell(Text(obj['serial'].toString(),textAlign: TextAlign.center,)),
+                            DataCell(Text(obj['date'].toString())),
+                            DataCell(
+                                areaBox(obj['Enumeration_Area'].toString())),
+                            DataCell(Text(obj['Household_Number'].toString(),textAlign: TextAlign.center,)),
+                            DataCell(Text(obj['Type'].toString())),
+                            DataCell(Text(obj['Male'].toString())),
+                            DataCell(Text(obj['Female'].toString())),
+                            DataCell(areaBox(obj['Status'].toString())),
+                            DataCell(Icon(
+                              Icons.edit,
+                              size: 15,
+                              color: Colors.grey,
+                            )),
+                          ],
+                        ),
+                      )
+                      .toList(),
 
-                  ],
-                  rows: [
-                    DataRow(cells: [
-                      DataCell(Text('1')),
-                      DataCell(Text('22.02.2022')),
-                      DataCell(Text('1')),
-                      DataCell(Text('045')),
-                      DataCell(Text('General')),
-                      ///////
-                      DataCell(Text('1')),
-                      DataCell(Text('22.02.2022')),
-                      DataCell(Text('1')),
-                      DataCell(Text('045')),
-                      DataCell(Text('General')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('2')),
-                      DataCell(Text('23.02.2022')),
-                      DataCell(Text('2')),
-                      DataCell(Text('066')),
-                      DataCell(Text('Institutional')),
-                      /////////////
-                      DataCell(Text('2')),
-                      DataCell(Text('23.02.2022')),
-                      DataCell(Text('2')),
-                      DataCell(Text('066')),
-                      DataCell(Text('Institutional')),
-                    ]),
-                  ],
+                  // DataRow(cells: [
+                  //   DataCell(Text('1')),
+                  //   DataCell(Text('22.02.2022')),
+                  //   DataCell(Text('1')),
+                  //   DataCell(Text('045')),
+                  //   DataCell(Text('General')),
+                  //   ///////
+                  //   DataCell(Text('1')),
+                  //   DataCell(Text('22.02.2022')),
+                  //   DataCell(Text('1')),
+                  //   DataCell(Text('045')),
+                  //   DataCell(Text('General')),
+                  // ]),
+                  // DataRow(cells: [
+                  //   DataCell(Text('2')),
+                  //   DataCell(Text('23.02.2022')),
+                  //   DataCell(Text('2')),
+                  //   DataCell(Text('066')),
+                  //   DataCell(Text('Institutional')),
+                  //   /////////////
+                  //   DataCell(Text('2')),
+                  //   DataCell(Text('23.02.2022')),
+                  //   DataCell(Text('2')),
+                  //   DataCell(Text('066')),
+                  //   DataCell(Text('Institutional')),
+                  // ]),
                 ),
               ),
             ),
--- lib/styles/table_style.dart
+++ lib/styles/table_style.dart
@@ -4,7 +4,33 @@ const Color tableStrokeColor = Color.fromRGBO(0, 140, 68, 0.2);
 const Color tableRowFillColor = Color.fromRGBO(0, 140, 68, 0.03);
 const Color tableHeadingRowFillColor = Color.fromRGBO(0, 140, 68, 0.8);
 const Color tableDataTextColor = Color.fromRGBO(38, 38, 38, 1);
+const Color areaBoxColor = Color.fromRGBO(38, 38, 38, 1);
 const borderSide = BorderSide(
   width: 2,
-  color:tableStrokeColor ,
+  color: tableStrokeColor,
+);
+BoxDecoration boxDecoration =
+    BoxDecoration(border: Border.all(color: areaBoxColor));
+
+//Enumeration Area Box
+Center areaBox(String value) => Center(
+  child:   Container(
+
+        decoration: BoxDecoration(
+
+          border: Border.all(color: areaBoxColor),
+
+          borderRadius: BorderRadius.circular(3),
+
+        ),
+
+    child: Padding(
+
+      padding: const EdgeInsets.all(2.0),
+
+      child: Text(value),
+
+    ),
+
+      ),
 );
